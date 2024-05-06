402d2eb91b5d778da64d408bf779ab4f3b318daf
Thiago Feijó <thiago.feijo13@gmail.com>
2024-05-06 15:15:40 +0000

    fix: folder path jest

--- jest.config.js
+++ jest.config.js
@@ -4,13 +4,13 @@ const config = {
   clearMocks: true,
   testEnvironment: 'jsdom',
   moduleNameMapper: {
-    '\\.s?css$': '<rootDir>/tests/fileMock.js',
+    '\\.s?css$': '<rootDir>/.config/tests/fileMock.js',
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/tests/fileMock.js',
+      '<rootDir>/.config/tests/fileMock.js',
     'src/(.*)': '<rootDir>/src/$1',
-    'tests/(.*)': '<rootDir>/tests/$1',
+    'tests/(.*)': '<rootDir>/.config/tests/$1',
   },
-  setupFilesAfterEnv: ['<rootDir>/tests/setupTests.js'],
+  setupFilesAfterEnv: ['<rootDir>/.config/tests/setupTests.js'],
   testMatch: ['**/?(*.)*(spec|test).[tj]s?(x)'],
 };
 
